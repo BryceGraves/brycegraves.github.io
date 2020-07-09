ccc89c896ea6331d0fd1ab5f3c0433935272127a
Bryce Graves <brycegraves97@gmail.com>
2020-07-08 23:57:51 -0700

    App: test out switch routing for git deploy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './containers/Home';
 
 const App = () => {
   return (
-    <HashRouter>
-      <Route exact path="/" component={Home} />
-    </HashRouter>
+    <Switch>
+      <Route exact path="/test" component={Home} />
+    </Switch>
   );
 };
 
