c80aa1703413f27102bd2c9de51d52f6de679b3a
Bryce Graves <brycegraves97@gmail.com>
2020-07-17 11:11:47 -0700

    ArtProject: stub out art project page

--- src/containers/ArtProject.js
+++ src/containers/ArtProject.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import { Container, Segment } from 'semantic-ui-react';
+import { Container, Divider, Grid, Header, Icon, Image, List, Segment } from 'semantic-ui-react';
 
 const ArtProject = () => {
   return (
     <Container>
-      <Segment>HEY SOME ART SHOULD GO HERE</Segment>
+      <Header as="h2" icon textAlign="center">
+        <Icon name="paint brush" circular />
+        <Header.Content>Charley Harper</Header.Content>
+      </Header>
+      <Segment className="mx-1">
+        <List bulleted>
+          <List.Item>Birthday</List.Item>
+          <List.Item>Where was he born?</List.Item>
+          <List.Item>
+            Benefits
+            <List.List>
+              <List.Item href="#">Link to somewhere</List.Item>
+              <List.Item>Rebates</List.Item>
+              <List.Item>Discounts</List.Item>
+            </List.List>
+          </List.Item>
+          <List.Item>Warranty</List.Item>
+        </List>
+      </Segment>
+      <Divider />
     </Container>
   );
 };
